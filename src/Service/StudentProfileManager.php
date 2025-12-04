9d0e64530abaf803a5f2277b2b190aacfc095b24
Sean McMeans <sean.mcmeans@outlook.com>
2025-12-03 19:41:08 -0500

    Improved performance

--- src/Service/StudentProfileManager.php
+++ src/Service/StudentProfileManager.php
@@ -9,8 +9,12 @@ class StudentProfileManager {
 
     protected $currentUser;
     protected $entityTypeManager;
+    
+    // Property to hold the cached node
+    protected $loadedProfile = NULL;
+    // Add a flag to know if we already checked (in case they don't have a profile)
+    protected $profileChecked = FALSE;
 
-    // We inject dependencies here via the constructor
     public function __construct(AccountProxyInterface $currentUser, EntityTypeManagerInterface $entityTypeManager) {
         $this->currentUser = $currentUser;
         $this->entityTypeManager = $entityTypeManager;
@@ -20,25 +24,41 @@ class StudentProfileManager {
     * Returns the Student Profile Node for the current user.
     */
     public function getStudentProfileNode() {
+        // If we already looked this up, return the saved result immediately.
+        if ($this->profileChecked) {
+            return $this->loadedProfile;
+        }
+
         $uid = $this->currentUser->id();
+        
+        // Mark that we are checking now, so we don't run this again even if result is null
+        $this->profileChecked = TRUE;
+
         if ($uid == 0) {
             return NULL;
         }
 
+        // Database query to find the student profile node for this user
         $query = $this->entityTypeManager->getStorage('node')->getQuery();
         $nids = $query
             ->condition('type', 'student_profile')
             ->condition('field_student_account', $uid)
             ->range(0, 1)
-            ->accessCheck(TRUE) // Important in Drupal 10+ for security!
+            ->accessCheck(TRUE)
             ->execute();
 
         if (empty($nids)) {
+            // Save NULL so we don't query again
+            $this->loadedProfile = NULL;
             return NULL;
         }
 
         $nid = reset($nids);
-        return $this->entityTypeManager->getStorage('node')->load($nid);
+        
+        // Save the loaded node into the property
+        $this->loadedProfile = $this->entityTypeManager->getStorage('node')->load($nid);
+        
+        return $this->loadedProfile;
     }
 
 }
