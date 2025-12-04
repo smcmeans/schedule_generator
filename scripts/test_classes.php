cde6e0e5ee4d70d31d45c2dc800f62f8c74f3321
Sean McMeans <sean.mcmeans@outlook.com>
2025-12-03 21:19:37 -0500

    Added support for ordering classes by prerequisites, untested

--- scripts/test_classes.php
+++ scripts/test_classes.php
@@ -20,6 +20,15 @@ if (method_exists(ScheduleGenerator::class, 'get_all_classes')) {
     
     // Print results
     print_r($results);
+    // Additionally, test sorting by prerequisites
+    if (method_exists(ScheduleGenerator::class, 'sort_courses_by_number')) {
+        $sorted_results = ScheduleGenerator::sort_courses_by_number($results);
+        echo "------------------------------------------------\n";
+        echo "Sorted Results by Course Number:\n";
+        print_r($sorted_results);
+    } else {
+        echo "Function 'sort_courses_by_number' not found.\n";
+    }
 } else {
     echo "Function 'get_all_classes' not found. Make sure the module is enabled.\n";
 }
