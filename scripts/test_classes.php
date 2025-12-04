c5f612682d5702712b00afe838c8c7fa78fa1e34
Sean McMeans <sean.mcmeans@outlook.com>
2025-12-03 21:22:37 -0500

    Now searches for correct sort function

--- scripts/test_classes.php
+++ scripts/test_classes.php
@@ -21,13 +21,13 @@ if (method_exists(ScheduleGenerator::class, 'get_all_classes')) {
     // Print results
     print_r($results);
     // Additionally, test sorting by prerequisites
-    if (method_exists(ScheduleGenerator::class, 'sort_courses_by_number')) {
-        $sorted_results = ScheduleGenerator::sort_courses_by_number($results);
+    if (method_exists(ScheduleGenerator::class, 'sort_classes_by_prerequisite')) {
+        $sorted_results = ScheduleGenerator::sort_classes_by_prerequisite($results);
         echo "------------------------------------------------\n";
-        echo "Sorted Results by Course Number:\n";
+        echo "Sorted Results by Course Prerequisite:\n";
         print_r($sorted_results);
     } else {
-        echo "Function 'sort_courses_by_number' not found.\n";
+        echo "Function 'sort_classes_by_prerequisite' not found.\n";
     }
 } else {
     echo "Function 'get_all_classes' not found. Make sure the module is enabled.\n";
