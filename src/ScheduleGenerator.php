3690a9382f2801a9e18ba316b3024cac13378fb7
smcmeans <luminous@SeanLegion.localdomain>
2025-12-10 15:36:59 -0500

    Removed debug code for credit hours

--- src/ScheduleGenerator.php
+++ src/ScheduleGenerator.php
@@ -22,9 +22,6 @@ class ScheduleGenerator
   public static function get_desired_credit_hours(NodeInterface $studentProfile)
   {
     $credit_hours = $studentProfile->get('field_desired_credit_hours')->value;
-
-    return 15;
-
     return is_numeric($credit_hours) ? (int) $credit_hours : 12; // Default to 12 if not set
   }
 
